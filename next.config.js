bcbe8f3e61002cbbc5978c67f4c8c6e623316aa6
strawberryisamu <kiuchi-isamu@life-coach.co.jp>
2023-11-08 01:10:41 +0900

    config update

--- next.config.js
+++ next.config.js
@@ -1,21 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 
-// next.config.js
-// module.exports = {
-//     experimental: {
-//       appDir: true,
-//     },
-//   };
-  
-// /** @type {import('next').NextConfig} */
-
 const branchName = process.env.BRANCH_NAME ? "/" + process.env.BRANCH_NAME : "";
 
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: branchName,
   basePath: branchName,
+  experimental: {
+    appDir: true,
+  },
 };
 
 module.exports = nextConfig;
